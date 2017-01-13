9b4470eed360a011cf619f5696f314735e92ce7e
marcomedina <mmedinalr@gmail.com>
2017-01-13 13:41:55 -0800

    Everyday at 11 AM

--- clock.js
+++ clock.js
@@ -2,7 +2,7 @@ var CronJob = require('cron').CronJob;
 var bot = require('./bot.js');
 
 new CronJob({
-    cronTime: '* * * * *', // this is a job that runs on weekdays at 11am
+    cronTime: '00 00 11 * * 1-5', // this is a job that runs on weekdays at 11am
     onTick: bot.sendMessage(),
     start: true,
     timeZone: 'America/Los_Angeles'
