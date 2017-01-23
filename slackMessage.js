ffc546a74f4fd352571478df46e9fd2d88fd2010
marcomedina <mmedinalr@gmail.com>
2017-01-23 10:51:32 -0800

    Private Channel

--- slackMessage.js
+++ slackMessage.js
@@ -3,6 +3,6 @@ var Bot = require('slackbots');
 module.exports = {
     send: function(channelName, message, attachments = []) {
         var bot = new Bot({token: process.env.SLACK_TOKEN});
-        bot.postMessageToChannel(channelName, message, {attachments: attachments, as_user: true});
+        bot.postMessageToGroup(channelName, message, {attachments: attachments, as_user: true});
     }
 };
